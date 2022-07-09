4436b0644b328abd014c0ff89d84c78c9ab67cd3
Shamanthshetty7 <shamanthshetty51@gmail.com>
2022-07-09 13:47:19 +0530

    beginning

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+//print" hello world' in java
+
+// class name and the program file(stored as .class extension)always should be same.
+
+public class classname{
+    public static void main(String[] args){
+    System.out.println("Hello World");
+    }
+}
+
+//execution always starts from main...
+//all the operations are same as c and c++ ...only syntax difference you can see including some inner process difference.
