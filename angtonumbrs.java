073c3adf5fe9237a6814ae5571d0a657bb2a46f6
Shamanthshetty7 <shamanthshetty51@gmail.com>
2022-07-16 18:31:21 +0530

    Create angtonumbrs.java

---
+++ angtonumbrs.java
@@ -0,0 +1,14 @@
+//Add two numbers
+
+//Consider the following code for adding two numbers
+class classname{
+public static void main(String[] args) {
+int num1 = 10;
+int num2 = 5;
+int ans = num1 + num2;
+System.out.println("Sum =" +ans);
+}
+}
+
+//Output:
+//15
