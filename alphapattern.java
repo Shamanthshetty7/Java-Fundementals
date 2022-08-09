f01ed061324033f821822e0260002dacc1dc0c76
Shamanthshetty7 <shamanthshetty51@gmail.com>
2022-08-09 12:08:48 +0530

    Alpha Pattern

---
+++ alphapattern.java
@@ -0,0 +1,33 @@
+// Code : Alpha Pattern
+
+// Print the following pattern for the given N number of rows.
+// Pattern for N = 3
+// A
+// BB
+// CCC
+
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int i=1;
+        while(i<=n){
+            int j=1;
+            while(j<=i)
+            {
+                char ch=(char)('A'+i-1);
+                System.out.print(ch);
+                j=j+1;
+            }
+            System.out.println();
+            i=i+1;
+        }
+
+		
+	}
+
+}
